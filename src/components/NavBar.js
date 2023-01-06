63bfbdb3395d22a0ceca88fd45589b4c9c2b0b35
Shraddha Tiwari <114010000+Shraddha08-cmyk@users.noreply.github.com>
2023-01-06 18:09:13 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-
-export const NavBar = () =>{
-    return(
-
-    )
-}
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+          <li >
+            <Link to="/home" className="home-link">Home Page</Link>
+          </li>
+        <li>
+          <Link to="/"  className="index-link">Index Page</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default NavBar;
