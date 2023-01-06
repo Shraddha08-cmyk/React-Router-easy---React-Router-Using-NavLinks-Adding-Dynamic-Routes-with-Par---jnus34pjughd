c5034aecdbaeaabebcf64b09695084070538bc32
Shraddha Tiwari <114010000+Shraddha08-cmyk@users.noreply.github.com>
2023-01-06 18:07:58 +0530

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,5 @@
-import React from 'react';
-
-export const NotFound = () => {
-    return (
-       
-    )
-}
+import React from "react";
+const NotFound = () => {
+  return <div id="not-found-page">Not found page</div>;
+};
+export default NotFound;
