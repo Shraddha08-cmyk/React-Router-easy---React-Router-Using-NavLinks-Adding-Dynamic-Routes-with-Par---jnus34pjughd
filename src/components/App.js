b673fc2e5201780f49e853d14b744f1dfc6d9680
Shraddha Tiwari <114010000+Shraddha08-cmyk@users.noreply.github.com>
2023-01-06 18:08:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
-import { AppRoutes } from '../AppRoutes';
+import  AppRoutes  from '../AppRoutes';
 import '../styles/App.css';
-import { NavBar } from './NavBar';
+import  NavBar  from './NavBar';
 const App = () => {
 
   return (
